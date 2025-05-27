265d2226a18f05bc8a5cc3b7b25afeb8299d1092
Mohammad Hossein Abbaspour <74285750+MohammadHAbbaspour@users.noreply.github.com>
2025-05-27 19:43:06 +0330

    edit run configuration for multi label dataset

--- refactor/ZeroShotExperimentTemplate.py
+++ refactor/ZeroShotExperimentTemplate.py
@@ -3,22 +3,27 @@ from config import *
 from experiment import *
 from agent import *
 import utils
+import os
 from openai import OpenAI
 client = OpenAI(api_key=utils.API_KEY)
-data = DataHandler('../datasets/number_pattern copy.tsv')
+data = DataHandler(os.path.join('..', 'datasets', 'multi-label-emotion.tsv'))
 
 def validator(predict):
-    return 'next_number' in predict and 'explanation' in predict and predict['next_number'].isdigit()
+    for l in predict['label_ids'].split(','):
+        if not (0 < int(l) < 29):
+            return False
+    return 'label_ids' in predict and 'explanation' in predict
 
 def extractor(predict):
-    result = {'predict':None, 'F2':None}
-    result['predict'] = predict['next_number']
-    result['F2'] = predict['explanation']
-    return result
+    result = {}
+    label_ids = list(map(int, predict['label_ids'].split(',')))
+    result['predict'] = label_ids
+    result['explanation'] = predict['explanation']
+    return result   
 
 config = ZeroShotConfig(utils.PERSONA, utils.INSTRUCTION, utils.DEBATE_OUTPUT_SCHEMA, validator, extractor)
 agent = ZeroShotLlm('gpt-4o-mini', client, config)
-basepath = '../experiments/number_pattern/dev3'
+basepath = os.path.join('..','experiments','multi-emotion','init-test')
 experiment = ZeroShotExperiment(data, agent, basepath)
 
 experiment.run()
